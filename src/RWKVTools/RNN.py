b85b8a07d0ec0dc45c05b38bb1a60d8d3d247c0f
Harrison <harrison.vanderbyl@gmail.com>
2023-09-03 18:43:28 +1000

    cleanup and fix

--- src/RWKVTools/RNN.py
+++ src/RWKVTools/RNN.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import os, math, gc, importlib
 from torch import nn
+import torch
 from torch.nn import functional as F
 import pytorch_lightning as pl
 from pytorch_lightning.utilities import rank_zero_info, rank_zero_only
@@ -80,195 +81,196 @@ try:
         def __init__(self, *args: Any, **kwargs: Any) -> None:
             super().__init__(*args, **kwargs)
 
-    @property
-    def deepspeed_offload(self) -> bool:
-        strategy = self.trainer.strategy
-        if isinstance(strategy, DeepSpeedStrategy):
-            cfg = strategy.config["zero_optimization"]
-            return cfg.get("offload_optimizer") or cfg.get("offload_param")
-        return False
-    def configure_optimizers(self):
-        args = self.args
+        @property
+        def deepspeed_offload(self) -> bool:
+            strategy = self.trainer.strategy
+            if isinstance(strategy, DeepSpeedStrategy):
+                cfg = strategy.config["zero_optimization"]
+                return cfg.get("offload_optimizer") or cfg.get("offload_param")
+            return False
         
-        lr_decay = set()
-        lr_1x = set()
-        lr_2x = set()
-        lr_3x = set()
-        for n, p in self.named_parameters():
-            if ("time_mix" in n) and (args.layerwise_lr > 0):
-                if args.my_pile_stage == 2:
-                    lr_2x.add(n)
-                else:
-                    lr_1x.add(n)
-            elif ("time_decay" in n) and (args.layerwise_lr > 0):
-                if args.my_pile_stage == 2:
-                    lr_3x.add(n)
-                else:
-                    lr_2x.add(n)
-            elif ("time_faaaa" in n) and (args.layerwise_lr > 0):
-                if args.my_pile_stage == 2:
-                    lr_2x.add(n)
-                else:
-                    lr_1x.add(n)
-            elif ("time_first" in n) and (args.layerwise_lr > 0):
-                lr_3x.add(n)
-            elif (len(p.squeeze().shape) >= 2) and (args.weight_decay > 0):
-                lr_decay.add(n)
-            else:
-                lr_1x.add(n)
-
-        lr_decay = sorted(list(lr_decay))
-        lr_1x = sorted(list(lr_1x))
-        lr_2x = sorted(list(lr_2x))
-        lr_3x = sorted(list(lr_3x))
-        # print('decay', lr_decay)
-        # print('1x', lr_1x)
-        # print('2x', lr_2x)
-        # print('3x', lr_3x)
-        param_dict = {n: p for n, p in self.named_parameters()}
-        
-        if args.layerwise_lr > 0:
-            if args.my_pile_stage == 2:
-                optim_groups = [
-                    {"params": [param_dict[n] for n in lr_1x], "weight_decay": 0.0, "my_lr_scale": 1.0},
-                    {"params": [param_dict[n] for n in lr_2x], "weight_decay": 0.0, "my_lr_scale": 5.0},# test: 2e-3 / args.lr_init},
-                    {"params": [param_dict[n] for n in lr_3x], "weight_decay": 0.0, "my_lr_scale": 5.0},# test: 3e-3 / args.lr_init},
-                ]
-            else:
-                optim_groups = [
-                    {"params": [param_dict[n] for n in lr_1x], "weight_decay": 0.0, "my_lr_scale": 1.0},
-                    {"params": [param_dict[n] for n in lr_2x], "weight_decay": 0.0, "my_lr_scale": 2.0},
-                    {"params": [param_dict[n] for n in lr_3x], "weight_decay": 0.0, "my_lr_scale": 3.0},
-                ]
-        else:
-            optim_groups = [{"params": [param_dict[n] for n in lr_1x], "weight_decay": 0.0, "my_lr_scale": 1.0}]
-
-        if args.weight_decay > 0:
-            optim_groups += [{"params": [param_dict[n] for n in lr_decay], "weight_decay": args.weight_decay, "my_lr_scale": 1.0}]
-            if self.deepspeed_offload:
-                return DeepSpeedCPUAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adamw_mode=True, amsgrad=False)
-            return FusedAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adam_w_mode=True, amsgrad=False)
-        else:
-            if self.deepspeed_offload:
-                return DeepSpeedCPUAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adamw_mode=False, weight_decay=0, amsgrad=False)
-            return FusedAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adam_w_mode=False, weight_decay=0, amsgrad=False)
-        # return ZeroOneAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, weight_decay=0, amsgrad=False, cuda_aware=False)
-
-    def training_step(self, batch, batch_idx):
-        args = self.args
-        if args.my_qa_mask != 1:
-            idx, targets = batch
-            logits = self(idx)
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
-            # if '0' in os.environ["RWKV_MY_TESTING"]:
-            #     print('logits', logits)
-            #     torch.set_printoptions(threshold=10000)
-            #     print('idx', idx)
-            #     exit(0)
-        else:
-            idx, targets, mask = batch
-            mask = mask.view(-1)
-            sum_mask = torch.sum(mask).item()
-            # if sum_mask == 0:
-            #     return torch.tensor([0.0], requires_grad=True)
-
-            logits = self(idx)
-            if sum_mask == mask.shape[0]:
-                loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
-                # print('rank', self.global_rank, 'loss', loss.item())
-            else:
-                loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), reduction='none')
-                # loss_raw = loss
-                loss = torch.sum(loss * mask) / sum_mask
-
-                # torch.set_printoptions(threshold=10000)
-                # if True: #self.global_rank == 1:
-                #     tmp = ''
-                #     sss = 0
-                #     ccc = 0
-                #     for i in range(mask.shape[0]):
-                #         if mask[i] > 0:
-                #             tmp += str(idx.view(-1)[i].item()) + ','
-                #             sss += loss_raw.view(-1)[i].float().item()
-                #             ccc += 1
-                #     print('rank', self.global_rank, 'loss', loss.item(), 'lavg', sss / ccc)#, 'tmp', tmp, 'input', idx)
-
-        return L2Wrap.apply(loss, logits)
-
-    def training_step_end(self, batch_parts):
-        all = self.all_gather(batch_parts)
-        if self.trainer.is_global_zero:
-            self.trainer.my_loss_all = all
-
-    def generate_init_weight(self):
-        print(
-            f"""
-############################################################################
-#
-# Init model weight (slow for large models)...
-#
-############################################################################
-"""
-        )
-        m = {}
-        for n in self.state_dict():
-            p = self.state_dict()[n]
-            shape = p.shape
-
-            gain = 1.0
-            scale = 1.0
-            if "ln_" in n or ".ln" in n or "time_" in n or "_mask" in n or "pos_emb" in n or '.mask.' in n or "model.1" in n or "model.3" in n:
-                if 'ln_x.weight' in n:
-                    layer_scale = (1+int(n.split('.')[1])) / self.args.n_layer
-                    m[n] = (p * 0.0) + (layer_scale ** 0.7)
-                else:
-                    m[n] = p
-            else:
-                if n == "model.0.weight":
-                    scale = -1 * self.args.lr_init
-                else:
-                    if shape[0] > shape[1]:
-                        gain = math.sqrt(shape[0] / shape[1])
-                    if 'r' in os.environ["RWKV_MY_TESTING"]:
-                        zero = [".att.output.", ".ffn.value.", ".ffn.receptance.", ".ffnPre.value.", ".ffnPre.receptance.", "head_q.", '.oo.', '.rr.']
+        def configure_optimizers(self):
+            args = self.args
+            
+            lr_decay = set()
+            lr_1x = set()
+            lr_2x = set()
+            lr_3x = set()
+            for n, p in self.named_parameters():
+                if ("time_mix" in n) and (args.layerwise_lr > 0):
+                    if args.my_pile_stage == 2:
+                        lr_2x.add(n)
                     else:
-                        zero = [".att.key.", ".att.receptance.", ".att.output.", ".ffn.value.", ".ffn.receptance.", ".ffnPre.value.", ".ffnPre.receptance.", "head_q.", '.oo.', '.rr.']
-                    for kk in zero:
-                        if kk in n:
+                        lr_1x.add(n)
+                elif ("time_decay" in n) and (args.layerwise_lr > 0):
+                    if args.my_pile_stage == 2:
+                        lr_3x.add(n)
+                    else:
+                        lr_2x.add(n)
+                elif ("time_faaaa" in n) and (args.layerwise_lr > 0):
+                    if args.my_pile_stage == 2:
+                        lr_2x.add(n)
+                    else:
+                        lr_1x.add(n)
+                elif ("time_first" in n) and (args.layerwise_lr > 0):
+                    lr_3x.add(n)
+                elif (len(p.squeeze().shape) >= 2) and (args.weight_decay > 0):
+                    lr_decay.add(n)
+                else:
+                    lr_1x.add(n)
+
+            lr_decay = sorted(list(lr_decay))
+            lr_1x = sorted(list(lr_1x))
+            lr_2x = sorted(list(lr_2x))
+            lr_3x = sorted(list(lr_3x))
+            # print('decay', lr_decay)
+            # print('1x', lr_1x)
+            # print('2x', lr_2x)
+            # print('3x', lr_3x)
+            param_dict = {n: p for n, p in self.named_parameters()}
+            
+            if args.layerwise_lr > 0:
+                if args.my_pile_stage == 2:
+                    optim_groups = [
+                        {"params": [param_dict[n] for n in lr_1x], "weight_decay": 0.0, "my_lr_scale": 1.0},
+                        {"params": [param_dict[n] for n in lr_2x], "weight_decay": 0.0, "my_lr_scale": 5.0},# test: 2e-3 / args.lr_init},
+                        {"params": [param_dict[n] for n in lr_3x], "weight_decay": 0.0, "my_lr_scale": 5.0},# test: 3e-3 / args.lr_init},
+                    ]
+                else:
+                    optim_groups = [
+                        {"params": [param_dict[n] for n in lr_1x], "weight_decay": 0.0, "my_lr_scale": 1.0},
+                        {"params": [param_dict[n] for n in lr_2x], "weight_decay": 0.0, "my_lr_scale": 2.0},
+                        {"params": [param_dict[n] for n in lr_3x], "weight_decay": 0.0, "my_lr_scale": 3.0},
+                    ]
+            else:
+                optim_groups = [{"params": [param_dict[n] for n in lr_1x], "weight_decay": 0.0, "my_lr_scale": 1.0}]
+
+            if args.weight_decay > 0:
+                optim_groups += [{"params": [param_dict[n] for n in lr_decay], "weight_decay": args.weight_decay, "my_lr_scale": 1.0}]
+                if self.deepspeed_offload:
+                    return DeepSpeedCPUAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adamw_mode=True, amsgrad=False)
+                return FusedAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adam_w_mode=True, amsgrad=False)
+            else:
+                if self.deepspeed_offload:
+                    return DeepSpeedCPUAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adamw_mode=False, weight_decay=0, amsgrad=False)
+                return FusedAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adam_w_mode=False, weight_decay=0, amsgrad=False)
+            # return ZeroOneAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, weight_decay=0, amsgrad=False, cuda_aware=False)
+
+        def training_step(self, batch, batch_idx):
+            args = self.args
+            if args.my_qa_mask != 1:
+                idx, targets = batch
+                logits = self(idx)
+                loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+                # if '0' in os.environ["RWKV_MY_TESTING"]:
+                #     print('logits', logits)
+                #     torch.set_printoptions(threshold=10000)
+                #     print('idx', idx)
+                #     exit(0)
+            else:
+                idx, targets, mask = batch
+                mask = mask.view(-1)
+                sum_mask = torch.sum(mask).item()
+                # if sum_mask == 0:
+                #     return torch.tensor([0.0], requires_grad=True)
+
+                logits = self(idx)
+                if sum_mask == mask.shape[0]:
+                    loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+                    # print('rank', self.global_rank, 'loss', loss.item())
+                else:
+                    loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), reduction='none')
+                    # loss_raw = loss
+                    loss = torch.sum(loss * mask) / sum_mask
+
+                    # torch.set_printoptions(threshold=10000)
+                    # if True: #self.global_rank == 1:
+                    #     tmp = ''
+                    #     sss = 0
+                    #     ccc = 0
+                    #     for i in range(mask.shape[0]):
+                    #         if mask[i] > 0:
+                    #             tmp += str(idx.view(-1)[i].item()) + ','
+                    #             sss += loss_raw.view(-1)[i].float().item()
+                    #             ccc += 1
+                    #     print('rank', self.global_rank, 'loss', loss.item(), 'lavg', sss / ccc)#, 'tmp', tmp, 'input', idx)
+
+            return L2Wrap.apply(loss, logits)
+
+        def training_step_end(self, batch_parts):
+            all = self.all_gather(batch_parts)
+            if self.trainer.is_global_zero:
+                self.trainer.my_loss_all = all
+
+        def generate_init_weight(self):
+            print(
+                f"""
+    ############################################################################
+    #
+    # Init model weight (slow for large models)...
+    #
+    ############################################################################
+    """
+            )
+            m = {}
+            for n in self.state_dict():
+                p = self.state_dict()[n]
+                shape = p.shape
+
+                gain = 1.0
+                scale = 1.0
+                if "ln_" in n or ".ln" in n or "time_" in n or "_mask" in n or "pos_emb" in n or '.mask.' in n or "model.1" in n or "model.3" in n:
+                    if 'ln_x.weight' in n:
+                        layer_scale = (1+int(n.split('.')[1])) / self.args.n_layer
+                        m[n] = (p * 0.0) + (layer_scale ** 0.7)
+                    else:
+                        m[n] = p
+                else:
+                    if n == "model.0.weight":
+                        scale = -1 * self.args.lr_init
+                    else:
+                        if shape[0] > shape[1]:
+                            gain = math.sqrt(shape[0] / shape[1])
+                        if 'r' in os.environ["RWKV_MY_TESTING"]:
+                            zero = [".att.output.", ".ffn.value.", ".ffn.receptance.", ".ffnPre.value.", ".ffnPre.receptance.", "head_q.", '.oo.', '.rr.']
+                        else:
+                            zero = [".att.key.", ".att.receptance.", ".att.output.", ".ffn.value.", ".ffn.receptance.", ".ffnPre.value.", ".ffnPre.receptance.", "head_q.", '.oo.', '.rr.']
+                        for kk in zero:
+                            if kk in n:
+                                scale = 0
+                        if n == "head.weight":
+                            scale = 0.5
+                        if "head_k." in n:
+                            scale = 0.1
+                        if "head_q." in n:
                             scale = 0
-                    if n == "head.weight":
-                        scale = 0.5
-                    if "head_k." in n:
-                        scale = 0.1
-                    if "head_q." in n:
-                        scale = 0
 
-                print(f"{str(shape[0]).ljust(5)} {str(shape[1]).ljust(5)} {str(scale).ljust(4)} {n}")
+                    print(f"{str(shape[0]).ljust(5)} {str(shape[1]).ljust(5)} {str(scale).ljust(4)} {n}")
 
-                if self.args.accelerator.upper() == "GPU":
-                    m[n] = torch.empty((shape[0], shape[1]), device="cuda")
-                else:
-                    m[n] = torch.empty((shape[0], shape[1]))
+                    if self.args.accelerator.upper() == "GPU":
+                        m[n] = torch.empty((shape[0], shape[1]), device="cuda")
+                    else:
+                        m[n] = torch.empty((shape[0], shape[1]))
 
-                if scale == 0:
-                    nn.init.zeros_(m[n])
-                elif scale < 0:
-                    nn.init.uniform_(m[n], a=scale, b=-scale)
-                else:
-                    nn.init.orthogonal_(m[n], gain=gain * scale)
+                    if scale == 0:
+                        nn.init.zeros_(m[n])
+                    elif scale < 0:
+                        nn.init.uniform_(m[n], a=scale, b=-scale)
+                    else:
+                        nn.init.orthogonal_(m[n], gain=gain * scale)
 
-            m[n] = m[n].cpu()
-            if os.environ["RWKV_FLOAT_MODE"] == "fp16":
-                m[n] = m[n].half()
-            elif os.environ["RWKV_FLOAT_MODE"] == "bf16":
-                m[n] = m[n].bfloat16()
+                m[n] = m[n].cpu()
+                if os.environ["RWKV_FLOAT_MODE"] == "fp16":
+                    m[n] = m[n].half()
+                elif os.environ["RWKV_FLOAT_MODE"] == "bf16":
+                    m[n] = m[n].bfloat16()
 
-            # if n == "emb.weight":
-            #     print(m[n])
+                # if n == "emb.weight":
+                #     print(m[n])
 
-        gc.collect()
-        torch.cuda.empty_cache()
-        return m
+            gc.collect()
+            torch.cuda.empty_cache()
+            return m
 
 except:
     pass
@@ -295,7 +297,6 @@ class TimeShift(nn.Module):
     def getState(self):
         return self.state.clone()
 
-import torch.functional as F
 
 # Short Memory
 class Short_Mem(nn.Module):
