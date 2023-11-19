c19f8e77a7686139b8db6a45e4fe1a9a292de56c
harrison <harrison.vanderbyl@gmail.com>
2023-11-18 18:38:02 -0800

    working

--- src/models/modules/quant/quant.cpp
+++ src/models/modules/quant/quant.cpp
@@ -2,56 +2,67 @@
 #include "ATen/ATen.h"
 #include <immintrin.h>
 #include <algorithm>
+#include <omp.h>
+// Helper function: Load 64 bytes with 16 floats, aligned to 64 byte boundary.
+inline __m512 load_aligned(const float* ptr) {
+    return _mm512_load_ps(ptr);
+}
 
-void matmul_avx512I(torch::Tensor &At, torch::Tensor &Art, torch::Tensor &Aot, torch::Tensor &Bt, torch::Tensor &Ct, long BB, long M, long N, long K) {
-    auto A = At.data_ptr<u_char>();
+__m512 convert_uint8_to_ps(const u_char* data) {
+    // Load the uint8 data into a 128-bit register as integer values
+    __m128i input = _mm_loadu_si128((const __m128i*)data);
+
+    // Zero extend 8-bit integers to 32-bit integers and then to single-precision floats
+    // AVX-512 provides the _mm512_cvtepu8_epi32 which directly extends uint8 values to 32-bit integers
+    __m512i extended = _mm512_cvtepu8_epi32(input);
+
+    // Convert extended 32-bit integers to single-precision floats
+    __m512 result = _mm512_cvtepi32_ps(extended);
+
+    return result;
+}
+// Helper function: Load 64 bytes with 16 floats, potentially unaligned.
+inline __m512 load_unaligned(const float* ptr) {
+    return _mm512_loadu_ps(ptr);
+}
+
+// Helper function: Store 64 bytes with 16 floats, aligned to 64 byte boundary.
+inline void store_aligned(float* ptr, __m512 value) {
+    _mm512_store_ps(ptr, value);
+}
+
+void matmul_avx512_optimized(const torch::Tensor &At, const torch::Tensor &Art, const torch::Tensor &Aot,
+                             const torch::Tensor &Bt, torch::Tensor &Ct,
+                             const long BB, const long IN, const long T, const long OUT) {
+    // Pointers to the data
+    auto A = At.data_ptr<unsigned char>();
     auto Ar = Art.data_ptr<float>();
     auto Ao = Aot.data_ptr<float>();
     auto B = Bt.data_ptr<float>();
     auto C = Ct.data_ptr<float>();
 
-    // Cache-blocking sizes, for example purposes. It should be tuned according to your CPU's cache sizes.
-    const long block_size_m = 64;
-    const long block_size_k = 256;
-    const long block_size_n = 64;
+    for (long i = 0; i < OUT; i += 1) {
+        long io = i << 4;
+        __m512 Ario = load_aligned(&Ar[io]);
+        __m512 Aoio = load_aligned(&Ao[io]);
+        for (long k = 0; k < IN; k += 16) {
+            __m512 aa = convert_uint8_to_ps(&A[i * IN + k]);
+            for (long j = 0; j < T; j += 1) {
+                for (long bb = 0; bb < BB; bb += 1) {
+                    
+                    __m512 a = _mm512_fmadd_ps(Ario, aa, Aoio);
+                         
+                    __m512 b = load_aligned(&B[bb * IN * T + j * IN + k]);
+                    __m512 c = _mm512_mul_ps(a, b);
 
-    #pragma omp parallel for collapse(2) schedule(dynamic)
-    for (long bb = 0; bb < BB; bb++) {
-        for (long mj0 = 0; mj0 < M; mj0 += block_size_m) {
-            for (long nj0 = 0; nj0 < N; nj0 += block_size_n) {
-                for (long j = nj0; j < std::min(nj0 + block_size_n, N); j += 16) {
-                    for (long i = mj0; i < std::min(mj0 + block_size_m, M); i++) {
-                        // Load the elements of C once, work on a block and store it back
-                        __m512 c_block[block_size_n / 16];
-                        for (long jj = j; jj < j + 16; jj += 16) {
-                            c_block[(jj - j) / 16] = _mm512_load_ps(&C[bb * N * M + i * N + jj]);
-                        }
+                    // atomic_add(&C[bb * T * OUT + i * T + j], c);
 
-                        for (long k0 = 0; k0 < K; k0 += block_size_k) {
-                            // Prepare blocks for A and B in the registers
-                            for (long k = k0; k < std::min(k0 + block_size_k, K); k++) {
-                                // Load and broadcast values of A and precomputed arrays
-                                long precomp_index = k % 16;
-                                __m512 a = _mm512_set1_ps(float(A[i * K + k]) * Ar[i * 16 + precomp_index] + Ao[i * 16 + precomp_index]);
-
-                                for (long jj = j; jj < j + 16; jj += 16) {
-                                    __m512 b = _mm512_load_ps(&B[bb * K * N + k * N + jj]);
-                                    c_block[(jj - j) / 16] = _mm512_fmadd_ps(a, b, c_block[(jj - j) / 16]);
-                                }
-                            }
-                        }
-
-                        // Store the results back to the memory
-                        for (long jj = j; jj < j + 16; jj += 16) {
-                            _mm512_store_ps(&C[bb * N * M + i * N + jj], c_block[(jj - j) / 16]);
-                        }
-                    }
+                    C[bb * T * OUT + j * OUT + i] += c[0] + c[1] + c[2] + c[3] + c[4] + c[5] + c[6] + c[7] + c[8] + c[9] + c[10] + c[11] + c[12] + c[13] + c[14] + c[15];
                 }
             }
         }
     }
 }
-
 void Quantize(torch::Tensor &At, torch::Tensor &Art, torch::Tensor &Aot, torch::Tensor &Aqt, long M, long N) {
     float* A = At.data_ptr<float>();
     float* Ar = Art.data_ptr<float>();
@@ -90,11 +101,11 @@ void Quantize(torch::Tensor &At, torch::Tensor &Art, torch::Tensor &Aot, torch::
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("quantize_cpu", &Quantize, "QuantizeCpu");
-    m.def("matmul", &matmul_avx512I, "matmul_avx512I");
+    m.def("matmul", &matmul_avx512_optimized, "matmul_avx512I");
     
 }
 
 TORCH_LIBRARY(wkv5, m) {
     m.def("quantize_cpu", Quantize);
-    m.def("matmul", matmul_avx512I);
+    m.def("matmul", matmul_avx512_optimized);
 }
--- src/models/modules/quant/testquant.py
+++ src/models/modules/quant/testquant.py
@@ -6,29 +6,33 @@ import time
 
 from torch.utils.cpp_extension import load
 quant_cpp = load(name="quant_cpp", sources=["./quant.cpp"], verbose=True,
-        extra_cflags=["-O3", "-march=native"])
+        extra_cflags=["-O3", "-march=native", "-fopenmp"  ,"-flto",  "-fopenmp", "-funroll-loops", "-D_GLIBCXX_PARALLEL"])
 
 class InferenceLinear(torch.nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__()
-        self.weight = (torch.rand(1024,1024))
-        self.qweight = (torch.zeros(1024,1024).to(torch.uint8))
-        self.qrange = (torch.zeros(1024,16))
-        self.qmin = (torch.zeros(1024,16))
-        quant_cpp.quantize_cpu(self.weight, self.qrange, self.qmin,self.qweight, 1024,1024)
+        weight = (torch.rand(4096,2048))
+        self.weight = weight
+        self.qweight = (torch.zeros(weight.shape[1],weight.shape[0]).to(torch.uint8))
+        self.qrange = (torch.zeros(weight.shape[1],16))
+        self.qmin = (torch.zeros(weight.shape[1],16))
+        quant_cpp.quantize_cpu(self.weight.t().contiguous(), self.qrange, self.qmin,self.qweight, self.weight.shape[1], self.weight.shape[0])
     def forward(self, x):
         # replicate nn.linear
         intime = time.time()
-        print((self.weight @ x).reshape(-1)[-10:])
+        print((x @ self.weight)[-1,:,-5:])
         outtime = time.time()
         print("Pytorch time: ", outtime-intime)
-        intime = time.time()
-        out = torch.empty(x.shape[0] , x.shape[1], self.weight.shape[1])
+        out = torch.zeros(x.shape[0] , x.shape[1], self.qweight.shape[0])
 
-        quant_cpp.matmul(self.qweight, self.qrange, self.qmin, x, out, x.shape[0], 1024, 1024, 1024)
+        intime = time.time()
+        
+        quant_cpp.matmul(self.qweight, self.qrange, self.qmin, x, out, x.shape[0], self.qweight.shape[1], x.shape[1], self.qweight.shape[0])
         outtime = time.time()
-        print(out.reshape(-1)[-10:])
+        print(out[-1,:,-5:])
         print("Quantized time: ", outtime-intime)
+
+        print("Difference: ", ((x @ self.weight) - out ).abs().max())
     
     def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs):
         key = list(state_dict.keys())[0]
@@ -38,5 +42,5 @@ class InferenceLinear(torch.nn.Module):
 
 quant = InferenceLinear()
 
-x = torch.randn(100,1024,1024)
+x = torch.randn(8,8,4096)
 quant(x)
